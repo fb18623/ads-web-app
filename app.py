8fc182cb4cbdfccb7a81add536352e0a17e15e08
lily <ds18788@bristol.acuk>
2021-04-19 17:01:23 +0100

    first

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,14 +12,37 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+app.title = 'Sentiment Towards Lockdown in the UK'
+
 app.layout = html.Div([
-    html.H2('UK COVID-19 Twitter Sentiment!'),
+    html.H2('UK COVID-19 Twitter Sentiment'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['England', 'Wales', 'Scotland']],
         value='England'
     ),
     html.Div(id='display-value')
+])
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
